d44605afa96d3fdddbc7bc8fc73ff37c96a6369d
Jayveer Singh <jay.veer1@protonmail.com>
2026-03-05 23:58:03 +0000

    Revert "Fix broken CMS images and move server env vars to amplify.yml"
    This reverts commit c3b1109cdbdc252c809fd68a55bbd783e6f07c08.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  env: {
+    PREVIEW_SECRET: process.env.PREVIEW_SECRET,
+    STRAPI_URL: process.env.STRAPI_URL,
+    STRAPI_API_TOKEN: process.env.STRAPI_API_TOKEN,
+  },
   images: {
     remotePatterns: [
       {
@@ -13,12 +18,6 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "koursair-media.s3.us-east-1.amazonaws.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "koursair-media-709141.s3.us-east-1.amazonaws.com",
         port: "",
         pathname: "/**",
       },
