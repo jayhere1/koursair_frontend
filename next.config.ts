36c5571169c3d74c2fd5a3debb54c58e7d393375
Jayveer Singh <jay.veer1@protonmail.com>
2026-03-12 13:02:24 +0000

    Remove duplicate remotePatterns entry in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -21,12 +21,6 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
-      {
-        protocol: "https",
-        hostname: "koursair-media-996549485205-ap-south-1.s3.ap-south-1.amazonaws.com",
-        port: "",
-        pathname: "/**",
-      },
     ],
   },
 };
